134e6ba7a87e4d40f28fb03c8899fd129f5dacad
Rachel Leggett <rleggett@umich.edu>
2015-12-07 15:10:04 -0500

    em for not in plugin repo line

--- plugins.php
+++ plugins.php
@@ -5,7 +5,7 @@
 <h3>Customized WordPress Plugins</h3>
 
 <div class="two-col">
-    <p>These plugins were written for <a href="http://caen.engin.umich.edu/web/publishing" target="_blank">CAEN's Web Publishing Service</a> and are not in the Wordpress.org plugin repository.</p>
+    <p><em>These plugins were written for <a href="http://caen.engin.umich.edu/web/publishing" target="_blank">CAEN's Web Publishing Service</a> and are not in the Wordpress.org plugin repository.i</em></p>
 	<div class="left">
 		<h4>Usage Reports</h4>
 		<p>Usage Reports is a plugin written for <a href="http://caen.engin.umich.edu/web/publishing" target="_blank">CAEN's Web Publishing Service</a> displaying information about their multisite usage on the network admin dashboard.  It displays:</p>
