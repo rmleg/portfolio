0d0a9e11a167fe8dd91c55356793a6a3b4aba3f7
Rachel Leggett <rleggett@umich.edu>
2015-12-07 15:37:09 -0500

    Updates description of Usage Reports

--- plugins.php
+++ plugins.php
@@ -8,7 +8,7 @@
     <p><em>These plugins were written for <a href="http://caen.engin.umich.edu/web/publishing" target="_blank">CAEN's Web Publishing Service</a> and are not in the Wordpress.org plugin repository.i</em></p>
 	<div class="left">
 		<h4>Usage Reports</h4>
-		<p>Usage Reports is a plugin written for <a href="http://caen.engin.umich.edu/web/publishing" target="_blank">CAEN's Web Publishing Service</a> displaying information about their multisite usage on the network admin dashboard.  It displays:</p>
+		<p>This plugin combines WordPress functions and SQL to query the database behind CAEN's 150+ site multisite network to display information about usage on the network admin dashboard.  It displays:</p>
 			<uL>
 				<li>Active plugins per site.</li>
 				<li>Contact info (from the custom field) for each site.</li>
@@ -21,11 +21,11 @@
 				<li>A list of users for each site with each admin user starred and admin email listed.</li>
 				<li>Active widgets per site.</li>
 			</ul>
-
 	</div>
         <div class="right">
                 <h4>Profiles</h4>
                 <p>This plugin leverages Custom Post Types, the Shortcode API, and the <a href="https://github.com/WebDevStudios/cmb2" target="_blank">CMB2</a> library to create a Profile type, simplifying the addition of people to listings within the College of Engineering WordPress network.</p> 
+
 		<h4>Hide Custom Fields</h4>
 		<p>Hide Custom Fields is a plugin that uses Javascript with JQuery to hide unused custom fields on the "edit page" screen in the dashboard, based on the currently-selected page template.</p>
 	</div>
