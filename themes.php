d48cf90a1e17e18027ced65b0baee25a96a8daf7
Rachel Leggett <rleggett@umich.edu>
2016-06-26 18:52:20 -0400

    Updated slider with newer work

--- themes.php
+++ themes.php
@@ -8,20 +8,16 @@
 
 
 					<ul class="bxslider">
+                                          <li><img src="images/screenshots/robotics.png" />
+                                                <p>&nbsp;</p>
+                                                <p>I built this responsive theme to be used by College of Engineering entities with ties to other schools.  The goal was to deviate slightly from the <a href="http://www.engin.umich.edu" target="_blank">main CoE brand</a> to show that these users are not solely tied to the College of Engineering.  This was accomplished using mockups for another department and modifying them to fit the needs of this service. <a href="http://robotics.umich.edu" target="_blank">Click here to view the theme in use.</a></p>
+                                          <p>&nbsp;</p></li>
+                                          <li><img src="images/screenshots/bergermarks.png" />
+                                                <p>&nbsp;</p>
+<p>I was contracted to build this responsive WordPress theme from PDF mockups created by a designer.  I also installed and configured the appropriate plugins to fit their needs. <a href="http://bergermarks.org" target="_blank">Click here to view the theme in use.</a></p>
+                                          <p>&nbsp;</p></li>
 					  <li><img src="images/screenshots/newtheme-responsive-right.png" />
 					  	<p>&nbsp;</p>
-					  	<p>I built this fully-responsive blog theme using starter theme <a href="http://underscores.me" target="_blank">Underscores</a> as a base.  Underscores gave me the functions to make my blog work great, and I gave it the style to make my blog look great.  This theme has several different color options and other customizations available in the Theme Customizer.  <a href="http://blog.rachelleggett.com" target="_blank">View a Live Demo</a></p>
+					  	<p>I built this fully-responsive blog theme using starter theme <a href="http://underscores.me" target="_blank">Underscores</a> as a base for much of the functionality.  I added customization options for color schemes and a sidebar image using the Customizer API. <a href="http://blog.rachelleggett.com" target="_blank">Click here to view the theme in use.</a></p>
 					  <p>&nbsp;</p></li>
-					  <li><img src="images/screenshots/burst-responsive-right.png" />
-					  	<p>&nbsp;</p>
-					  	<p>I built this fully-responsive blog theme from scratch.  It is a bare-bones WordPress theme and was my first responsive design.<!--  <a href="http://rachel.nu" target="_blank">View a Live Demo</a>--></p>
-					  	<p>&nbsp;</p></li>
-					  <li><img src="images/screenshots/sophistique.png" />
-					  	<p>&nbsp;</p>
-					  	<p>This non-responsive theme is highly stylized for a personal blog with focus on content and interesting flourishes.  This is not something I would release today, but I like to showcase it for the artistic design elements.  <a href="sophistique.htm" target="_blank">View a Live Demo</a></p>
-					  	<p>&nbsp;</p></li>
-					  <li><img src="images/screenshots/mrun.png" />
-					  	<p>&nbsp;</p>
-					  	<p>I designed this website for the Michigan Running Club during my time as their webmaster.  They needed a simple website that would offer plenty of information for perspective runners as well as for current club runners and their families.</p>
-					  	<p>&nbsp;</p></li>
 					</ul>
