6356097f2fca4d8e506dc10e814e039540b1ca76
Rachel Leggett <leggett.rachel@gmail.com>
2020-08-02 18:47:08 -0400

    New about blurb

--- v2/src/components/about.js
+++ v2/src/components/about.js
@@ -2,6 +2,7 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "../utils/fontawesome"
 import ProfilePic from "../images/RachelLeggettProfilePhoto.png"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
@@ -14,7 +15,40 @@ const About = () => {
       </div>
       <div id="about-text">
         <h1>Rachel Leggett</h1>
-        <span className="social">
+        <p>
+          Hi, I'm Rachel! 👋 I work at{" "}
+          <a
+            href="https://engin.umich.edu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Michigan Engineering
+          </a>{" "}
+          as a Senior Full-Stack Web Developer in beautiful Ann Arbor, Michigan.
+          I work with Python, Flask, PHP, Phalcon, JavaScript, React, Vue, and
+          MySQL, specializing in a11y.
+        </p>
+        <p>
+          I also own a knitting pattern design business called{" "}
+          <a
+            href="https://arbortwist.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Arbor Twist
+          </a>
+          . I teach classes about gauge and sock knitting at my local yarn
+          store. I build online tools for knitters.
+        </p>
+        <p>
+          I pole dance, I have four cats, and my favorite TV show is Buffy the
+          Vampire Slayer.
+        </p>
+        <p>
+          You can find me online at <Link to="/links">these other places.</Link>{" "}
+          ↗️
+        </p>
+        {/* <span className="social">
           <a
             href="https://www.linkedin.com/in/rmleg/"
             target="_blank"
@@ -39,7 +73,7 @@ const About = () => {
           >
             <FontAwesomeIcon icon={["fab", "twitter"]} className="grow" />
           </a>
-        </span>
+        </span> */}
       </div>
     </div>
   )
