4d1091c062e6e34ca366b0e0267b4627337934fe
Rachel Leggett <leggett.rachel@gmail.com>
2020-08-09 17:29:37 -0400

    Updates email in footer

--- v2/src/components/footer.js
+++ v2/src/components/footer.js
@@ -4,7 +4,7 @@ export default () => (
   <footer>
     <p id="contact">
       Want to chat about my work, or just get in touch? Email me at{" "}
-      <a href="mailto:leggett.rachel@gmail.com">leggett.rachel@gmail.com</a>.
+      <a href="mailto:hi@rachelleggett.com">hi@rachelleggett.com</a>.
     </p>
     <p id="copyright">
       &copy; Rachel Leggett
